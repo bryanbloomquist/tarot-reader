71ef02b8141db16be1c2596b179c799f980094a7
bloomy215 <bmbloomquist@gmail.com>
2020-01-09 11:24:14 -0600

    run build

---
+++ build/precache-manifest.e6ce7b3da3f47d05f41ddc9ed2094132.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "655541d664ad8f662b0bf2bf37456d56",
+    "url": "/tarot-reader/index.html"
+  },
+  {
+    "revision": "26d2954fb38ec4c03feb",
+    "url": "/tarot-reader/static/css/main.62859c50.chunk.css"
+  },
+  {
+    "revision": "0bb0d9185ee8152765ce",
+    "url": "/tarot-reader/static/js/2.b72f976c.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/tarot-reader/static/js/2.b72f976c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26d2954fb38ec4c03feb",
+    "url": "/tarot-reader/static/js/main.c3264bef.chunk.js"
+  },
+  {
+    "revision": "5c7cf829f6c968bebe00",
+    "url": "/tarot-reader/static/js/runtime-main.f65665f2.js"
+  }
+]);
