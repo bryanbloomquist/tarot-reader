7725185bdecd3c0a785a3d76b7714cd8bf38eeb0
bloomy215 <bmbloomquist@gmail.com>
2020-01-08 23:33:02 -0600

    Updates

---
+++ precache-manifest.03ac10b119f32ec94ecacaad011f1cbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6423498823f9aeccae83ee6bc62bfd2d",
+    "url": "/bryanbloomquist.github.io/tarot-reader/index.html"
+  },
+  {
+    "revision": "fd1b8074091eab815bd0",
+    "url": "/bryanbloomquist.github.io/tarot-reader/static/css/main.fb0189f6.chunk.css"
+  },
+  {
+    "revision": "0bb0d9185ee8152765ce",
+    "url": "/bryanbloomquist.github.io/tarot-reader/static/js/2.b72f976c.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/bryanbloomquist.github.io/tarot-reader/static/js/2.b72f976c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd1b8074091eab815bd0",
+    "url": "/bryanbloomquist.github.io/tarot-reader/static/js/main.5b2bc533.chunk.js"
+  },
+  {
+    "revision": "4a79b3ac2f7128bf0844",
+    "url": "/bryanbloomquist.github.io/tarot-reader/static/js/runtime-main.934b541b.js"
+  }
+]);
